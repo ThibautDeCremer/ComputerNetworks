af10f9745ce3083c185d638d1a3105ca6aad3d93
NielsVerdijck <36706799+NielsVerdijck@users.noreply.github.com>
2019-03-14 17:28:55 +0100

    Update Client.java

--- Assignment1_AdBlocker/src/Client.java
+++ Assignment1_AdBlocker/src/Client.java
@@ -6,15 +6,12 @@
 
 import java.net.*;
 import java.util.Scanner;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.*;
 
 /**
- * to do:
+ * TODO:
  * 	- GET image in processImages ok?
  * 	- store HTML locally and store found image files locally -> how?
- * 	- is ad blocker correct? (the implementation is a bit naive)
  * 	- I have not yet tested the PUT and POST functions
  */
 
@@ -31,61 +28,238 @@ public class Client
 	 */
 	public static void main(String[] args) throws Exception
 	{
-		if (args.length != 2) // length of the command should be 2: command and host
+		if (args.length != 2) // length of the command should be 2: command and host(+path)
 		{
 			System.out.println("Not a valid command");
 			System.out.println("Done");
 			return;
 		}
 		
-		if ((args[0].equals("GET")) || (args[0].equals("HEAD")))
+		if (args[0].equals("HEAD"))
 		{
-			Socket sock = new Socket(InetAddress.getByName(args[1]),80); // open socket with default port 80
+			String host = null;
+			String path = "/";
+			
+			if (args[1].contains("/")) // if-else statement to retrieve the host and path
+			{
+				int index = args[1].indexOf("/");
+				int l = args[1].length();
+				
+				host = args[1].substring(0, index);
+				path = args[1].substring(index, l);
+			}
+			
+			else
+				host = args[1];
+			
+			Socket sock = new Socket(InetAddress.getByName(host),80); // open socket with default port 80
 			PrintWriter pw = new PrintWriter(sock.getOutputStream(),true);
-			BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-			pw.println(args[0] +  " / HTTP/1.1"); // as specified in the assignment, client program should support HTTP version 1.1
-			pw.println("Host: " + args[1]);
+			pw.println("HEAD " + path +" HTTP/1.1"); // as specified in the assignment, client program should support HTTP version 1.1
+			pw.println("Host: " + host);
 			pw.println("Connection: Close"); // close connection after making request
 			pw.println(); // always end with blank line
-			Boolean connectionLength = false;
-			String incomming = br.readLine() + "\r\n";
+			BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			String incoming = "";
+			String r = br.readLine() + "\r\n";
+			incoming += r;
 			
-			while(! connectionLength)
+			while(! r.equals(""))
+			{
+				r = br.readLine();
+				incoming += r + "\r\n";
+			}
+			
+			System.out.println(incoming);
+			br.close();
+			sock.close();
+			System.out.println("Done");
+		}
+		
+		else if (args[0].equals("GET"))
+		{
+			String host = null;
+			String path = "/";
+			
+			if (args[1].contains("/")) // if-else statement to retrieve the host and path
+			{
+				int index = args[1].indexOf("/");
+				int l = args[1].length();
+				
+				host = args[1].substring(0, index);
+				path = args[1].substring(index, l);
+			}
+			
+			else
+				host = args[1];
+			
+			Socket sock = new Socket(InetAddress.getByName(host),80); // open socket with default port 80
+			PrintWriter pw = new PrintWriter(sock.getOutputStream(),true);
+			pw.println("GET " + path +" HTTP/1.1"); // as specified in the assignment, client program should support HTTP version 1.1
+			pw.println("Host: " + host);
+			pw.println("Connection: Keep-Alive"); // keep connection alive after making request
+			pw.println(); // always end with blank line
+			
+			/*// test
+			InputStream so = sock.getInputStream();
+			byte[] b = new byte[4000];
+			int i = so.read(b);
+			// test*/
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			String header = "";
+			String r = br.readLine() + "\r\n";
+			header += r;
+			
+			while(!r.equals("")) // parse header
+			{
+				r = br.readLine();
+				header+= r + "\r\n";
+			}
+			
+			System.out.println(header);
+			String html = "";
+			
+			if (header.contains("Content-Length"))
+			{
+				String ConLen = "Content-Length";
+				int cijfer = header.indexOf(ConLen);
+				int endIn = header.indexOf("\r\n", cijfer);
+				int leng = Integer.parseInt(header.substring(cijfer+(ConLen.length())+2,endIn));
+				int test = 0;
+				
+				// keep reading until all content is retrieved (Content-Length is given)
+				while (test < leng)
+				{
+					String t = br.readLine();
+					
+					if (t == null) // TODO: check why final print of test does not equal content-length, last <html> tag is printed (de-cremer.be/webpage/)
+						break; // test with example.com not successful, test is at most 1220 in stead of 1270
+					
+					test += t.length();
+					System.out.println(test);
+					html = html + t + "\r\n";
+					//leng -= (t.length()+2); // \r\n is only 2 characters
+				}
+				System.out.println(test);
+			}
+			
+			else if (header.contains("chunked"))
+			{
+				/*String h = br.readLine(); // NOT GOOD, to simple
+				html += h + "\r\n";
+				System.out.println(html.length());
+				System.out.println(h);
+				
+				while(h != "0")
+				{
+					h = br.readLine();
+					html += h + "\r\n";
+					System.out.println(html.length());
+					System.out.println(h);
+				}*/
+				
+				
+				String h = br.readLine();
+				System.out.println(h);
+				int counter = Integer.parseInt(h,16);
+				System.out.println(counter);
+				Boolean fullTransfer = false;
+				
+				while(!fullTransfer)
+				{
+					while(counter >= -1)
+					{
+						String s = br.readLine();
+						System.out.println(s);
+						html += s;
+						counter -= (s.length()+2);
+						System.out.println(counter);
+						if (counter > 0)
+							html += "\r\n";
+						System.out.println(html.length());
+					}
+					
+					String k = br.readLine();
+					System.out.println(k);
+					counter = Integer.parseInt(k,16);
+					if (counter == 0)
+					{
+						fullTransfer = true;
+						html += "\r\n";
+					}
+				}
+			}
+			
+			else // we only accept transfer of data specified with content-Length or chunked transfer
+			{
+				System.out.println("Incoming content not with content-length or chunked");
+				System.out.println("Done");
+			}
+			
+			/*while(! connectionLength)
 			{
 				pw.println(args[0] +  " / HTTP/1.1"); // as specified in the assignment, client program should support HTTP version 1.1
 				pw.println("Host: " + args[1]);
 				String r;
 				r = br.readLine();
-				incomming = incomming + r + "\r\n";
-				
-				if (r.contains("Content-Length") || (r == null))
-					connectionLength = true;
-				
-				while(r != null)
+				incoming = incoming + r + "\r\n";
+								
+				while(r.equals("")) // parse header
 				{
 					r = br.readLine();
-					incomming+= r + "\r\n";
+					incoming+= r + "\r\n";
 					if ((r == null)|| r.contains("Content-Length"))
 						connectionLength = true;
-
 				}
+				
+				if (incoming.contains("Content-Length") || (r == null))
+					connectionLength = true;
+
+			}*/
+			
+			if (scanImages(html))
+			{
+				html = processImages(html, host, path, sock);
 			}
 			
-			incomming = incomming.replace("null", "");
+			FileWriter fw = new FileWriter("AdBlock.html");
+			BufferedWriter bw = new BufferedWriter(fw);
+			try
+			{
+				bw.write(html);
+				bw.newLine();
+				bw.close();
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
 			
-			if (scanImages(incomming))
-				incomming = processImages(incomming);
-			
-			System.out.println(incomming);
+			System.out.println(html);
 			br.close();
 			sock.close();
 			System.out.println("Done"); // needed to see whether or not the program is still running
 			return;
 		}
 		
-		else if ((args[0].equals("PUT")) || (args[0].equals("POST")))
+		else if ((args[0].equals("PUT")) || (args[0].equals("POST"))) // TODO: voeg content-length toe aan upload
 		{
-			Socket sock = new Socket(InetAddress.getByName(args[1]),80);
+			String host = null;
+			String path = "/";
+			
+			if (args[1].contains("/")) // if-else statement to retrieve the host and path
+			{
+				int index = args[1].indexOf("/");
+				int l = args[1].length();
+				
+				host = args[1].substring(0, index);
+				path = args[1].substring(index, l);
+			}
+			
+			else
+				host = args[1];
+			
+			Socket sock = new Socket(InetAddress.getByName(host),80);
 			BufferedWriter wr = new BufferedWriter( new OutputStreamWriter(sock.getOutputStream(),"UTF8"));
 			Boolean bool = true;
 			while (bool)
@@ -93,10 +267,11 @@ public class Client
 				System.out.println("Enter something to upload in server: ");
 				Scanner scanner = new Scanner(System.in);
 				String input = scanner.nextLine();
-				String write = URLEncoder.encode(input,"UTF-8");
-				wr.write(args[0] + " / HTTP/1.1\r\n");
-				wr.write("Content-Length: " + input.length());
-				wr.write("Content-Type: application/x-www-form-urlencoded\r\n");
+				String write = URLEncoder.encode(input,"UTF-8"); // www works with UTF-8
+				wr.write(args[0] + " " + path + "HTTP/1.1");
+				wr.write("Host: " + host);
+				wr.write("Content-Type: text/html"); //application/x-www-form-urlencoded\r\n
+				wr.write("Content-Length: " + write.length() + "\r\n");
 				wr.write("\r\n");
 				wr.write(write);
 				wr.flush();
@@ -152,8 +327,10 @@ public class Client
 	 * @throws IOException 
 	 * @throws UnknownHostException 
 	 */
-	private static String processImages(String allImages) throws UnknownHostException, IOException
+	private static String processImages(String allImages, String host, String path, Socket socket) throws UnknownHostException, IOException
 	{
+		PrintWriter pr = new PrintWriter(socket.getOutputStream(),true);
+
 		int index = allImages.indexOf("<img ", 0); // function returns -1 if no index can be found
 		
 		while(index != -1)
@@ -163,18 +340,37 @@ public class Client
 			String image = allImages.substring(beginIndex, endIndex);
 			if (image.contains("ad")) // if the found embedded image is an add, replace it with something else.
 			{
-				image = "ReplacementPicture.png";
-				allImages.replace(allImages.substring(beginIndex, endIndex),image);
+				image = "ReplacementPicture.png"; //TODO: find how to get this picture
+				allImages = allImages.replace(allImages.substring(beginIndex, endIndex),image);
 			}
 			// GET operation to retrieve the image and store it locally -> perhaps use same function as in the main
-			Socket s = new Socket(InetAddress.getByName(image),80);
-			PrintWriter pr = new PrintWriter(s.getOutputStream(),true);
-			pr.println("GET / HTTP/1.1");
-			pr.println("Host: " + image);
-			pr.println("Connection: Close");
+			
+			if (image.substring(0, 4) == "http")
+			{
+				String im = image.substring(7,image.length()); // http://.........
+				int ind = im.indexOf("/");
+				host = im.substring(0, ind);
+				image = im.substring(ind,im.length());
+			}
+			System.out.println("TEST");
+
+			pr.println("GET " + path + "/" + image + " HTTP/1.1");
+			pr.println("Host: " + host);
+			pr.println("Connection: Keep-Alive");
 			pr.println();
-			BufferedImage img = ImageIO.read(s.getInputStream()); // now need to store this
-			s.close();
+			
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			int len = in.readInt(); // length of the incoming image
+			byte[] im = new byte[len];
+			if (len > 0)
+			{
+				in.readFully(im,0,im.length); // read the entire image
+			}
+			try (FileOutputStream fos = new FileOutputStream("pathname")) // TODO: replace pathname
+			{
+				 fos.write(im);
+			}
+			// not allowed : BufferedImage img = ImageIO.read(socket.getInputStream());
 			
 			index = allImages.indexOf("<img ", endIndex);
 		}
