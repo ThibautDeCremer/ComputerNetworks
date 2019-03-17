cb4a71f1fae7fa570c8863406c759f1a7cc4d52f
NielsVerdijck <36706799+NielsVerdijck@users.noreply.github.com>
2019-03-17 15:24:37 +0100

    Update Client.java

--- Assignment1_AdBlocker/src/Client.java
+++ Assignment1_AdBlocker/src/Client.java
@@ -16,38 +16,41 @@ public class Client
 	 * Main running program of client.
 	 * 
 	 * @param args
-	 * 		Array with input. The input array should at least contain 2 elements.
-	 * 		- First element in array is the HTTP command
-	 * 		- Second element in array is the host name of the server
+	 * 		Array with input. The input array should at least contain 2 elements, it may at most contain 3 elements.
+	 * 		- First element in array is the HTTP command (GET, HEAD, PUT or POST).
+	 * 		- Second element in array is the host name of the server (+path).
+	 * 		- Third element in array is the number of the port. If no port is specified, default port 80 is used.
+	 * 
 	 * @throws Exception
+	 * 		When an I/O error occurs when storing the incoming HTML file.
 	 */
 	public static void main(String[] args) throws Exception
 	{
-		if ((args.length < 2) || (args.length > 3)) // length of the command should be 2 or 3: command and host(+path) (+port)
+		if ((args.length < 2) || (args.length > 3)) // length of the command should be 2 or 3: command + host(and path) (+port)
 		{
 			System.out.println("Not a valid command");
 			System.out.println("Done");
 			return;
 		}
 		
-		int port = 80;
+		int port = 80; // default port
 		
-		if (args.length == 3)
+		if (args.length == 3) // extract the port from the input argument
 		{
 			try
 			{
 				port = Integer.parseInt(args[2],10);
 			}
-			catch (NumberFormatException e)
+			catch (NumberFormatException e) // if the third part of the input array is not a number, a NumberFormatException is thrown.
 			{
-				System.out.println("The port should be the 3 part of the input string");
+				System.out.println("The port should be the third part of the input string");
 				System.out.println("Done");
 				return;
 			}
 		}
 		
 		/**
-		 * part of the method that deals with HEAD requests. (WORKS)
+		 * part of the method that deals with HEAD requests.
 		 */
 		if (args[0].equals("HEAD"))
 		{
@@ -66,7 +69,7 @@ public class Client
 			else
 				host = args[1];
 			
-			Socket sock = new Socket(InetAddress.getByName(host),port); // open socket with default port 80
+			Socket sock = new Socket(InetAddress.getByName(host),port); // open socket
 			PrintWriter pw = new PrintWriter(sock.getOutputStream(),true);
 			pw.println("HEAD " + path +" HTTP/1.1"); // as specified in the assignment, client program should support HTTP version 1.1
 			pw.println("Host: " + host);
@@ -78,7 +81,7 @@ public class Client
 			String r = br.readLine() + "\r\n";
 			incoming += r;
 			
-			while(! r.equals(""))
+			while(! r.equals("")) // loop until first blank line, that is the end of the header
 			{
 				r = br.readLine();
 				incoming += r + "\r\n";
@@ -110,7 +113,7 @@ public class Client
 			else
 				host = args[1];
 			
-			Socket sock = new Socket(InetAddress.getByName(host),port); // open socket with default port 80
+			Socket sock = new Socket(InetAddress.getByName(host),port); // open socket
 			PrintWriter pw = new PrintWriter(sock.getOutputStream(),true);
 			String req = "GET " + path +" HTTP/1.1\r\n";
 			req += "Host: " + host + "\r\n";
@@ -118,7 +121,7 @@ public class Client
 			req += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n";
 			req += "Accept-Language: nl,en-US;q=0.7,en;q=0.3\r\n";
 			req += "Connection: keep-alive\r\n";
-			req += "Upgrade-Insecure-Requests: 1\r\n";
+			req += "Upgrade-Insecure-Requests: 1\r\n"; // send the entire header of the request in one go (speed up of request)
 			pw.println(req);
 			
 			InputStream inSt = sock.getInputStream();
@@ -127,7 +130,7 @@ public class Client
 			String r = br.readLine() + "\r\n";
 			header += r;
 			
-			while(!r.equals("")) // parse header
+			while(!r.equals("")) // parse header to know how the content is transfered (chunked or content-length)
 			{
 				r = br.readLine();
 				header+= r + "\r\n";
@@ -158,16 +161,16 @@ public class Client
 			{
 				String h = br.readLine();
 				int counter = Integer.parseInt(h,16);
-				//Boolean fullTransfer = false; // check whether or not there has been a full transfer of data.
-				//char[] cbuf = new char[1] ;
-				//while(!fullTransfer)
-				while (counter > 0)
+				Boolean fullTransfer = false; // check whether or not there has been a full transfer of data.
+				char[] cbuf = new char[1] ;
+				while(!fullTransfer)
+				//while (counter > 0)
 				{
 					/**
-					 * If I run this code, it doesn't work.
+					 * If I run this code (in comments), it doesn't work.
 					 * But it works while debugging with a breakpoint before char[] cbuf = new ...
 					 */
-					char[] cbuf = new char[counter];
+					/*char[] cbuf = new char[counter];
 					br.read(cbuf, 0, counter);
 					String t = new String(cbuf);
 					html += t;
@@ -181,18 +184,13 @@ public class Client
 					{
 						h = br.readLine();
 						counter = Integer.parseInt(h,16);
-					}
-					/*while(counter > 0)
+					}*/
+					while(counter > 0)
 					{
-						//String s = br.readLine();
 						br.read(cbuf, 0, 1);
 						System.out.print(Character.toString(cbuf[0]));
 						html += Character.toString(cbuf[0]);
 						counter --;
-						//System.out.println(counter);
-						//if (counter > 0)
-							//html += "\r\n";
-						//System.out.println(html.length());
 					}
 					
 					String k = br.readLine();
@@ -203,7 +201,7 @@ public class Client
 					{
 						fullTransfer = true;
 						html += "\r\n";
-					}*/
+					}
 				}
 			}
 			
@@ -215,10 +213,10 @@ public class Client
 									
 			if (scanImages(html))
 			{
-				html = processImages(html, host, path, port, sock);
+				html = processImages(html, host, path, port, sock); // invoke method that deals with all the images.
 			}
 			
-			FileWriter fw = new FileWriter("AdBlock.html"); // write to file
+			FileWriter fw = new FileWriter("AdBlock.html"); // write html to locally stored file named AdBlock.html
 			BufferedWriter bw = new BufferedWriter(fw);
 			try
 			{
@@ -332,21 +330,21 @@ public class Client
 	 * @return
 	 * 		Index of the first occurrence. -1 is returned if no such index can be found or the length of ba is smaller than 2 or bigger than byteArray.
 	 */
-	private static int byteSearch(byte[] byteArray, byte[] ba) // This works
+	private static int byteSearch(byte[] byteArray, byte[] ba)
 	{
 		int L = byteArray.length;
 		int l = ba.length;
 		
-		if ((l < 2) || (L < l))
+		if ((l < 2) || (L < l)) // The length of byteArray must not be smaller than the length of ba. For simplicity, the length of ba should be at least 2 (always true for this client)
 			return -1;
 		
 		for(int i=0; i < (L-l+1); i++)
 		{
-			if(byteArray[i] == ba[0])
+			if(byteArray[i] == ba[0]) // search for first hit
 			{
 				for(int j=1;j<l;j++)
 				{
-					if((byteArray[i+j] == ba[j]) && (j == l-1))
+					if((byteArray[i+j] == ba[j]) && (j == l-1)) // if first occurrence is found, return the index
 						return i;
 					else if(byteArray[i+j] != ba[j])
 						break;
@@ -358,10 +356,14 @@ public class Client
 	}
 	
 	/**
-	 * Method to deal with  all images in the HTML file. This method also implements the ad block.
+	 * Method to deal with  all images in the HTML file, retrieve them and store them locally. This method also implements the ad block.
 	 * 
 	 * @param allImages
 	 * 		String containing all HTML lines that reference images.
+	 * 
+	 * @return
+	 * 		Modified HTMl string if the original contained advertisements.
+	 * 
 	 * @throws Exception
 	 * 		If no header is found in the incoming byte array, an exception is thrown
 	 */
@@ -379,13 +381,12 @@ public class Client
 			String image = allImages.substring(beginIndex, endIndex);
 			if (image.contains("ad")) // if the found embedded image is an add, replace it with something else.
 			{
-				image = "ReplacementPicture.png";
-				allImages = allImages.replace(allImages.substring(beginIndex, endIndex),image);
+				image = "ReplacementPicture.png"; // this picture is stored locally
+				allImages = allImages.replace(allImages.substring(beginIndex, endIndex),image); // insert replacement picture where advertisement is in HTML string
 			}
-			// GET operation to retrieve the image and store it locally -> perhaps use same function as in the main
 			
 			/**
-			 * Not needed in the html file of webpage.
+			 * Not needed in the HTML file of webpage. These lines of code will retrieve an image from another host.
 			 */
 			if (image.substring(0, 4) == "http")
 			{
@@ -394,9 +395,8 @@ public class Client
 				host = im.substring(0, ind);
 				image = im.substring(ind,im.length());
 			}
-			//System.out.println("TEST");
 
-			if (image != "ReplacementPicture.png")
+			if (image != "ReplacementPicture.png") // if this image is not an advertisement, retrieve it.
 			{
 				String r = "GET " + path + image + " HTTP/1.1\r\n";
 				r += "Host: " + host + "\r\n";
@@ -406,14 +406,14 @@ public class Client
 				r += "Accept-Encoding: gzip, deflate\r\n";
 				r += "Referer: http://" + host+path + "\r\n";
 				r += "Connection: keep-alive\r\n";
-				pr.println(r);
+				pr.println(r); // send request in one go the speed up the request a bit.
 				
 				byte[] endOfHeader = {13,10,13,10}; // equals \r\n in bytes -> indicates end of header
 				int c = 0;
-				byte[] b = new byte[1];
-				byte[] he = new byte[0];
+				byte[] b = new byte[1]; // byte buffer for the next incoming byte
+				byte[] he = new byte[0]; // byte array storing the header
 				
-				while(c!=-1) // first try to parse the header
+				while(c!=-1) // first try to parse the header to know how big the image is
 				{
 					c = is.read(b, 0, 1); // read a byte at a time
 					
@@ -433,13 +433,13 @@ public class Client
 				String ConLen = "Content-Length";
 				int cijfer = headr.indexOf(ConLen);
 				int endIn = headr.indexOf("\r\n", cijfer);
-				int leng = Integer.parseInt(headr.substring(cijfer+(ConLen.length())+2,endIn));
+				int leng = Integer.parseInt(headr.substring(cijfer+(ConLen.length())+2,endIn)); // stores the length of the image
 				
 				int f = 0;
-				int che = 0;
-				byte[] tf = new byte[leng];
-				byte[] im = new byte[leng];
-				while ((che < leng) && (f != -1))
+				int che = 0; // check for the amount of read bytes of the image
+				byte[] tf = new byte[leng]; // byte buffer for the next bytes of the image
+				byte[] im = new byte[leng]; // byte array containing the image
+				while ((che < leng) && (f != -1)) // loop until whole image is retrieved
 				{
 					f = is.read(tf, 0, leng-che);
 					
@@ -451,10 +451,10 @@ public class Client
 				}
 				
 				ByteArrayInputStream ins = new ByteArrayInputStream(im);
-				BufferedImage bi = ImageIO.read(ins);
-				ImageIO.write(bi, "jpg", new File(image));
+				BufferedImage bi = ImageIO.read(ins); // convert byte array to buffered image, needed for next line
+				ImageIO.write(bi, "jpg", new File(image)); // store image locally
 			}
-			index = allImages.indexOf("<img ", endIndex);
+			index = allImages.indexOf("<img ", endIndex); // search for the next image in the html file
 		}
 		
 		return allImages;
